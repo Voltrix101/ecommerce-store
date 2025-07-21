1a6bf63d74232f02cd97e83d6cce138fc9f3ad45
Amrito Ballav Roy <amritob0327.roy@gmail.com>
2025-07-21 19:11:03 +0530

    Fix Button import case in ThankYouPage

--- src/pages/ThankYouPage.tsx
+++ src/pages/ThankYouPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { Button } from '../components/ui/button';
+import { Button } from '../components/ui/Button';
 import { useNavigate, useLocation } from 'react-router-dom';
 
 export const ThankYouPage: React.FC = () => {
